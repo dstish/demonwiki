59ee8f9a689fd450a04b8f9d81e342682c1c0716
Nikita Gromov <gromov.n@edu.narfu.ru>
2023-07-06 01:17:52 +0300

    added the ability to comment

--- demon_souls/forms.py
+++ demon_souls/forms.py
@@ -1,22 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
-from .item_models import Item
+from .item_models import Item, Comment
 from tinymce.widgets import TinyMCE
+
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'password1', 'password2']
 
+
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
 
+
 class ItemForm(forms.ModelForm):
     category = forms.ChoiceField(choices=Item.CATEGORIES)
-    description = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 10}))
+    description = forms.CharField(
+        widget=TinyMCE(attrs={'cols': 80, 'rows': 10}))
 
     class Meta:
         model = Item
         fields = ['name', 'description', 'image', 'category']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
--- demon_souls/item_models.py
+++ demon_souls/item_models.py
@@ -30,3 +30,14 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.item.name}'
