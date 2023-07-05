fd2c3ba4ff3c3ca2aa933a0cdb5894316e9fea11
Nikita Gromov <gromov.n@edu.narfu.ru>
2023-07-05 18:38:51 +0300

    trash2

--- demon_souls/admin.py
+++ demon_souls/admin.py
@@ -3,13 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from django.contrib import admin
 from .item_models import Item
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'category')
     list_filter = ('category',)
 
+
 admin.site.register(Item, ItemAdmin)
 
 # Определяем новый класс администратора для модели CustomUser
+
+
 class CustomUserAdmin(UserAdmin):
     # Определяем отображаемые поля модели в административной панели
     list_display = ('username', 'is_active', 'is_admin')
@@ -27,6 +32,7 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('username',)
     ordering = ('username',)
     filter_horizontal = ()
+
 
 # Регистрируем модель CustomUser и класс администратора в административной панели
 admin.site.register(CustomUser, CustomUserAdmin)
--- demonwiki/settings.py
+++ demonwiki/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'demonwiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
